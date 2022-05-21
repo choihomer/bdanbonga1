5ccf2c0f22a29de73798243a37e4d4bd9a938e62
choihomer <major@moaland.co.kr>
2022-05-21 18:29:25 +0900

    220521_2

--- src/App.js
+++ src/App.js
@@ -2,53 +2,46 @@
 import './App.css';
 import bgimg from './img/main1.jpg' ;
 import bglogo from './img/logo1.jpg' ;
+import { Button, Navbar, Container, Nav} from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot} from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Bride from './routes/bride.js'
+
 function App() {
+  let navigate = useNavigate() ;
   return (
     <div className="App Container">
-      <nav className="navbar navbar-expand-md bg-light">
-        <div className="container-fluid">
-          <img src={bglogo} width ="40px"/>
-          <a className="navbar-brand" href="#">비단본가</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#">신부한복</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">신랑한복</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">혼주한복</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">하객한복</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">찾아오시는길</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">관리자</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav> 
-      <div className="imgBox">
-        <img src={bgimg}></img>
-      </div>
-      <div></div>
+      <Navbar bg="black" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">
+          <img src={bglogo} width ="40px"/>비단본가
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link onClick={()=>{navigate('/')}} >홈으로</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('/bride')}}>신부한복</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('')}}>신랑한복</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('')}}>혼주한복</Nav.Link>
+          <Nav.Link onClick={()=>{navigate('')}}></Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+          <Route path='/' element={
+            <div className="imgBox">
+              <img src={bgimg}></img>
+            </div>
+         }/>
+          <Route path="/bride" element={<Bride/>} />
+      </Routes>
+      
       <footer>
         <div className="row mainFooter">
           <span className="col-lg-4 col-md-12">
             <FontAwesomeIcon icon={faPhone} /> 043-234-5165</span>
           <span className="col-lg-4 col-md-12"><FontAwesomeIcon icon={faLocationDot} /> 충청북도 청주시 서원구 성화로 101</span>
-          <span className="col-lg-4 col-md-12"><FontAwesomeIcon icon={faLocationDot} /> 충청북도 청주시 서원구 죽림동 328</span>
+          <span className="col-lg-4 col-md-12"><FontAwesomeIcon icon={faLocationDot} /> [지번] 충청북도 청주시 서원구 죽림동 328</span>
         </div>
       </footer>
     </div>
@@ -56,3 +49,6 @@ function App() {
 }
 
 export default App;
+
+//github에 자료가 안올라가고 오류남
+//local에 자꾸 폴더가 생김
---
+++ src/routes/bride.js
@@ -0,0 +1,9 @@
+function Bride(){
+    return(
+        <div>
+            <h2>신부화면</h2>
+        </div>
+    )
+}
+
+export default Bride ;
